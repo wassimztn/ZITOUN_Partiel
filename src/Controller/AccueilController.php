2ef7ace4bbe293d78aeb8c23f9a2c9437f4bfb6b
wassimztn <wassimztn@users.noreply.github.com>
2025-11-25 15:45:05 +0100

    aj

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class AccueilController extends AbstractController
+class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
+        // On passe éventuellement des variables au template
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'page_title' => 'Accueil',
         ]);
     }
 }
